a534328b1325ce6eaf4fa1e3c28fa802a711aa86
Rafael Oliveira <rsantana.oliveira@paypal.com>
2023-09-15 12:05:02 +0200

    =)

--- action/index.js
+++ action/index.js
@@ -9821,14 +9821,18 @@ async function run () {
     const response = await octokit.request("GET /repos/{owner}/{repo}/issues/{issue_number}/comments", {
       owner: github.context.repo.owner,
       repo: github.context.repo.repo,
-      issue_number: github.context.issue.number
+      issue_number: github.context.issue.number,
+      per_page: 100
     });
 
     core.info('Setting output...');
 
     if (response.status === 200) {
-      core.info(JSON.stringify(response.data));
-      core.setOutput('changelog-text', response.status);
+      const comments = response.data.json();
+
+      for (let comment in comments) {
+        core.info(comment['user']['type'] + ' ' + comment['body'])
+      }
     } else {
       core.setFailed('Could not download issue comments. Request ended with ' + response.status);
     }

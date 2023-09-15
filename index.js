6ba5fee552481f27d2bea6cb56bce2815f5b3052
Rafael Oliveira <rsantana.oliveira@paypal.com>
2023-09-15 09:00:38 +0200

    Fix

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const core = require('@actions/core');
 
 async function run() {
   try {
-    const issueId = core.getInput('issue-id');
-    core.setOutput('changelog-text', 'The issue id is: ' + issueId);
+    const issueNumber = core.getInput('issue-number');
+    core.setOutput('changelog-text', 'The issue id is: ' + issueNumber);
   } catch (error) {
     core.setFailed(error.message);
   }

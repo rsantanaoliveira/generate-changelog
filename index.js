a668c92d6b8ed2056d187abdd899519f4c2c71d8
Rafael Oliveira <rsantana.oliveira@paypal.com>
2023-09-15 09:05:00 +0200

    Fix

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ async function run() {
   try {
     const issueNumber = core.getInput('issue-number');
     core.setOutput('changelog-text', 'The issue id is: ' + issueNumber);
+    core.info('logging '+ issueNumber);
   } catch (error) {
     core.setFailed(error.message);
   }

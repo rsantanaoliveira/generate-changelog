bf00d4af0c169f93e6da7d3438cc4223dcc98963
Rafael Oliveira <rsantana.oliveira@paypal.com>
2023-09-15 12:15:06 +0200

    =)

--- action/index.js
+++ action/index.js
@@ -9828,7 +9828,9 @@ async function run () {
     core.info('Setting output...');
 
     if (response.status === 200) {
-      core.info(response.data[0].body);
+      for (const comment in response.data) {
+        core.info(comment.body);
+      }
     } else {
       core.setFailed('Could not download issue comments. Request ended with ' + response.status);
     }
